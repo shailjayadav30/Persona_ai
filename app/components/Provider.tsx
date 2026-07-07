cfa13531358aa1817a2edef75ba868c48b5f74cc
shailjayadav30 <shailjayadav7275@gmail.com>
2026-07-07 22:04:15 +0530

    signin

---
+++ app/components/Provider.tsx
@@ -0,0 +1,5 @@
+import { SessionProvider } from "next-auth/react";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
